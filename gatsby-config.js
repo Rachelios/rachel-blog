65749dfef0a63b53450c0cbd682ebf7c14a9686f
DESKTOP-ELSTNHT\Rachel Le <lenguyenbichngoc95@gmail.com>
2021-01-27 00:06:49 +0800

    First commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -98,7 +98,10 @@ module.exports = {
         }]
       }
     },
-    
+    // Including in your Gatsby plugins will transform any paths in your frontmatter
+    `gatsby-plugin-netlify-cms-paths`,
+
+    // Including in your Remark plugins will transform any paths in your markdown body
     {
       resolve: 'gatsby-transformer-remark',
       options: {
