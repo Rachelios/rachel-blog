4695e98e12ac7d9d1926a1754ecab424fb654914
Rachelios <lenguyenbichngoc95@gmail.com>
2021-06-26 21:25:08 +0800

    update

--- src/components/Post/Author/Author.js
+++ src/components/Post/Author/Author.js
@@ -17,7 +17,7 @@ const Author = () => {
           rel="noopener noreferrer"
           target="_blank"
         >
-          <strong>{author.name}</strong> on LinkedIn
+          <strong> {author.name} </strong>  on LinkedIn.
         </a>
       </p>
     </div>
